08129897ea70552fd57e19dfc9578df7ed893709
davidedelerma <davide.delerma@gmail.com>
2017-07-09 12:23:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+React - Redux
+===============
+
+This is a project to quickly setup react and redux.
+
+First, install the dependencies.
+
+```
+npm install
+```
+
+To build and serve the project locally, run:
+
+```
+npm run serve
+```
+
+To just build the project, run:
+
+```
+npm run build
+```
---
+++ src/app.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './components/App';
+import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+require('./index.html');
+
+// Create redux store
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Create app
+const container = document.getElementById('app-container');
+
+ReactDOM.render(
+  <AppContainer>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AppContainer>
+  , container
+);
+
+// Hot module reloading
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </AppContainer>
+      , container
+    );
+  });
+}
+
