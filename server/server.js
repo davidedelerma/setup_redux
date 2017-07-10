ea7b262b00e74dbc2198ef108bada39a3ff81ae0
davidedelerma <davide.delerma@gmail.com>
2017-07-10 22:33:43 +0100

    refactor to test vs git

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express()
 var path = require('path')
 
+//testing
+//visual studio git
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
